284e39eaa4e7a862e33f29ebf25a084ad24f1e1c
Mark <mark@remarkablemark.org>
2021-04-10 22:20:27 -0400

    feat(plugin): add hello-world.1d.js

---
+++ plugin/hello-world.1d.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env /usr/local/bin/node
+
+// <xbar.title>Hello, World!</xbar.title>
+// <xbar.version>v1.0.0</xbar.version>
+// <xbar.author>Menglin "Mark" Xu</xbar.author>
+// <xbar.author.github>remarkablemark</xbar.author.github>
+// <xbar.desc>Outputs "Hello, World!"</xbar.desc>
+// <xbar.dependencies>node</xbar.dependencies>
+// <xbar.abouturl>https://github.com/remarkablemark/xbar-hello-world#readme</xbar.abouturl>
+
+console.log("Hello, World!");
