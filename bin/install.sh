9f1bf22834b44487577e99e5f5bb6baab0958d99
Mark <mark@remarkablemark.org>
2021-04-11 16:03:59 -0400

    chore(bin): display message when plugin is symlinked

--- bin/install.sh
+++ bin/install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # git working directory
-GIT_WORKING_DIR="$(git rev-parse --show-toplevel)"
+GIT_WORKING_DIR=$(git rev-parse --show-toplevel)
 
 # get plugin filename
-PLUGIN_FILENAME="$(
+PLUGIN_FILENAME=$(
   find plugin -type f -name '*.js' |
   xargs basename
-)"
+)
 
-# create alias by symlinking plugin in plugin folder
+# symlink plugin in plugin folder
 ln -sf "$GIT_WORKING_DIR/plugin/$PLUGIN_FILENAME" "$HOME/Library/Application Support/xbar/plugins/$PLUGIN_FILENAME"
+
+echo "Symlinked '$PLUGIN_FILENAME' in plugin folder"
