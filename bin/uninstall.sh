9a9e3679d7abb9d25cd7766615a5b8fd10d38c5a
Mark <mark@remarkablemark.org>
2021-04-11 16:04:45 -0400

    chore(bin): display message when plugin is removed

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # get `{name}` from `{name}.{time}.{ext}`
-PLUGIN_NAME="$(
+PLUGIN_NAME=$(
   find plugin -type f -name '*.js' |
   xargs basename |
   awk -F '.' '{print $1}'
-)"
+)
 
-# delete file that matches plugin name
-find "$HOME/Library/Application Support/xbar/plugins/" -name "$PLUGIN_NAME*" -delete
+# find file that matches plugin name in plugin folder
+PLUGIN_PATH=$(find "$HOME/Library/Application Support/xbar/plugins" -name "$PLUGIN_NAME*")
+
+# delete file in plugin folder (if exists)
+if [[ -n "$PLUGIN_PATH" ]]; then
+  rm "$PLUGIN_PATH"
+  test $? -ne 0 || echo "Deleted '$PLUGIN_PATH'"
+fi
